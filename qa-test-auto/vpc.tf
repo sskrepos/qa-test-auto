975b429b9405674de87c23a51f2c93fdea96a43e
S Sai Kiran Vemulapalli <sai.vemulapalli_contractor@onedatascan.com>
2017-02-21 15:36:58 -0500

    created vpc.tf

---
+++ qa-test-auto/vpc.tf
@@ -0,0 +1,30 @@
+#--------------------------------------------------------------
+# This module creates all resources necessary for a VPC
+#--------------------------------------------------------------
+
+variable "vpc_name" {}
+
+variable "cidr" {}
+
+resource "aws_vpc" "vpc" {
+  cidr_block           = "${var.cidr}"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags {
+    Name = "${var.vpc_name}"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+output "vpc_id" {
+  value = "${aws_vpc.vpc.id}"
+}
+
+output "vpc_cidr" {
+  value = "${aws_vpc.vpc.cidr_block}"
+}
+
