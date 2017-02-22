0c8a8aadc44afe12dff402eb79f5cea15c383184
S Sai Kiran Vemulapalli <sai.vemulapalli_contractor@onedatascan.com>
2017-02-22 14:37:24 -0500

    variable and general vpc

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -2,29 +2,48 @@
 # This module creates all resources necessary for a VPC
 #--------------------------------------------------------------
 
-variable "vpc_name" {}
+provider "aws"{
+  access_key="xxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+  secret_key="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+  region = "us-east-1"
+}
 
-variable "cidr" {}
+variable "vpc_name" {
+
+  type = "string"
+
+  default = "qa-test-auto"
+
+}
+
+variable "cidr1" {
+
+  type = "string"
+
+  default = "10.70.0.0/21"
+
+}
+
+
+
+
+
 
 resource "aws_vpc" "vpc" {
-  cidr_block           = "${var.cidr}"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
 
-  tags {
-    Name = "${var.vpc_name}"
-  }
+        cidr_block = "${var.cidr}"
 
-  lifecycle {
-    create_before_destroy = true
-  }
+        tags{
+
+                Name = "${var.vpc_name}"
+
+}
+
 }
 
 output "vpc_id" {
-  value = "${aws_vpc.vpc.id}"
-}
 
-output "vpc_cidr" {
-  value = "${aws_vpc.vpc.cidr_block}"
+        value = "${aws_vpc.vpc.id}"
+
 }
 
